f3d3a3ef1d7f9172d1bc96419e70f940caa38eca
Ahmed-Programmer0 <ahmedwaelforcollage@gmail.com>
2025-12-11 08:33:25 +0200

    Ahmed Wael added the F# Records and Data Structures For Student and Courses

---
+++ Student_Grades_Management_System/ModelDesigner/recordsandDatastructure.fs
@@ -0,0 +1,49 @@
+module recordsandDatastructure
+//  Course Record With Labels And Thir Types
+type Course = {
+                CourseCode:string;
+                CourseName:string;
+                CourseGrade:int;
+                CourseAppreciation:char
+}
+
+// Admin Record With Labels and Their Types
+type Admin = {
+                AdminName : string;
+                NationalId: int;
+                AdminEmail: string;
+                AdminPassword: string
+}
+
+let Admin ={
+                AdminName = "Ahmed Wael";
+                NationalId= 40906070;
+                AdminEmail = "ahmed@gmail.com";
+                AdminPassword = "12345"
+}
+
+
+// The Discriminted Union Of AppreciationWord
+type WordOfAppreciation = 
+                        | A of excellent : string
+                        | B of veryGood  : string
+                        | C of Good      : string
+                        | D of acceptable: string
+                        | F of failed    : string
+
+// Student Record With Labels And Their Types
+// Note : Courses : list<Course> means That Courses holds a List of Course Objects defined Above 
+type Student = {
+                Id : int; 
+                Name : string; 
+                NationalId : int;
+                Email : string;
+                Password : string;
+                yearInCollege : int;
+                DateOfEnteringCollege : string;
+                Courses : list<Course>;
+                Gpa : float;
+                TotalGrade : int;
+                Appreciation : char;
+                AppreciationWord : WordOfAppreciation
+}
